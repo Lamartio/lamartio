b120693c742412a1c037b239556789167bc26975
Danny Lamarti <lamarti.danny@gmail.com>
2022-01-26 11:55:21 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+      title: `lamartio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "AIzaSyCB1zxR6D3f8ErfgMw3vn_jjRgeJFB9-0Q"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
