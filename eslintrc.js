dc1eb4146c77b7c9abca38f0dd5446d3e45f294c
Danny Lamarti <lamarti.danny@gmail.com>
2022-01-26 12:18:58 +0100

    Inital commit installs all gatsby with TS

---
+++ eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        browser: true
+    }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,38 @@
 module.exports = {
-  siteMetadata: {
-      title: `lamartio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "AIzaSyCB1zxR6D3f8ErfgMw3vn_jjRgeJFB9-0Q"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `lamartio`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                "trackingId": "AIzaSyCB1zxR6D3f8ErfgMw3vn_jjRgeJFB9-0Q"
+            }
+        },
+        "gatsby-plugin-typescript",
+        "gatsby-plugin-material-ui",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        }
+    ]
 };
